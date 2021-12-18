412692e7cb50c77217da1443603eabf7078e3aeb
MiniLuca <81662629+TheMiniLuca@users.noreply.github.com>
2021-12-18 15:55:28 +0900

    1.1.1
    버그 수정
    
    BUG FIX

--- plugin/src/main/java/com/github/theminiluca/clear/lag/plugin/Clearlag.java
+++ plugin/src/main/java/com/github/theminiluca/clear/lag/plugin/Clearlag.java
@@ -11,6 +11,7 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.logging.Logger;
 
 public class Clearlag extends JavaPlugin implements Listener {
@@ -26,10 +27,8 @@ public class Clearlag extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
         String packageName = this.getServer().getClass().getPackage().getName();
-        String version = packageName.substring(packageName.lastIndexOf('.') + 1);
-        this.version = version;
+        AtomicReference<String> version = new AtomicReference<>(packageName.substring(packageName.lastIndexOf('.') + 1));
         plugin = this;
-        logger.info(version);
         try {
             final Class<?> clazz = Class.forName("com.github.theminiluca.clear.lag.nms." + version + ".NMSHandler");
             if (NMS.class.isAssignableFrom(clazz)) {
@@ -49,7 +48,6 @@ public class Clearlag extends JavaPlugin implements Listener {
             return;
         }
         getServer().getPluginManager().registerEvents(this, this);
-        getServer().getPluginManager().registerEvents(this, this);
         new UpdateChecker(this, 98464).getLastVersion(var -> {
             if (this.getDescription().getVersion().equalsIgnoreCase(var)) {
                 logger.info("this is plugin latest version");
@@ -57,6 +55,7 @@ public class Clearlag extends JavaPlugin implements Listener {
                 logger.warning("this is plugin old version! please update! ( new version : " + var + " )");
                 logger.warning("https://www.spigotmc.org/resources/clearlag.98464/");
             }
+            this.version = var;
         });
 
     }
@@ -76,12 +75,10 @@ public class Clearlag extends JavaPlugin implements Listener {
         Player player = event.getPlayer();
         if (player.isOp() || player.getUniqueId().toString().equalsIgnoreCase("f47b3f97-4891-43e7-a5d3-5c3b98c2b3f7")) {
             player.sendMessage(ChatColor.GREEN + "[CLEARLAG] The plugin is operating on that server!");
-            new UpdateChecker(this, 98464).getLastVersion(var -> {
-                if (!this.getDescription().getVersion().equalsIgnoreCase(var)) {
-                    player.sendMessage(ChatColor.RED + "this is plugin old version! please update!");
-                    player.sendMessage(ChatColor.RED + "https://www.spigotmc.org/resources/clearlag.98464/");
-                }
-            });
+            if (!this.getDescription().getVersion().equalsIgnoreCase(this.version)) {
+                player.sendMessage(ChatColor.RED + "this is plugin old version! please update! ( new version : " + this.version + " )");
+                player.sendMessage(ChatColor.RED + "https://www.spigotmc.org/resources/clearlag.98464/");
+            }
 
         }
     }
